a028c88b47d8896a2240e5d67d1d6f297039cb5f
huddeldaddel <35447824+huddeldaddel@users.noreply.github.com>
2019-01-16 21:22:52 +0100

    Fixed issue #1

--- src/components/pr_label.jsx
+++ src/components/pr_label.jsx
@@ -4,7 +4,7 @@ import './pr_label.css';
 
 export default function PRLabel(props) {
   return (
-    <span key={props.label.id} style={{ 
+    <span className="pr-label" key={props.label.id} style={{ 
           backgroundColor: `#${props.label.color}`,
           color: "000000" === props.label.color ? "rgba(255, 255, 255, 0.86)" : "#000000" 
         }}>
